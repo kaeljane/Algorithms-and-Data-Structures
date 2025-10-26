e3094de57414082dde934f62eb56c2ced3fd2d6b
Kaeljane <kayyt0102@gmail.com>
2025-10-26 11:34:39 -0300

    Att 24/10

--- Lista_Encadeada/Lista_Encadeada.py
+++ Lista_Encadeada/Lista_Encadeada.py
@@ -1,4 +1,4 @@
-import No
+from No import No
 
 class ListaEncadeada:
     def __init__(self):
@@ -20,31 +20,110 @@ class ListaEncadeada:
     def tamanho(self, tamanho):
         self._tamanho = tamanho
     
+    # def consultar_inicio(self):
+    #     if self._inicio is None:
+    #         return None
+    #     else:
+    #         return self._inicio
     def consultar_inicio(self):
         if self._inicio is None:
             return None
         else:
             return self._inicio
+    def consultar_final(self):
+        if self._inicio is None:
+            return None
+        else:
+            atual = self._inicio
+            while (atual.prox is not None):
+                atual = atual.prox
+            return atual.prox
 
-    def consultar_final():
-        pass
-    def consultar_indice():
-        pass
+    def consultar_final(self):
+        if self._inicio is None:
+            return None
+        atual = self._inicio
+        while (atual.prox is not None):
+            atual = atual.prox
+        return atual.prox # lembrar de sempre imprimir o valor que seria esse atual.prox
+        
+    def consultar_indice(self, posicao):
+        if self._inicio is None:
+            return None
+        # para evitar erros
+        if posicao < 1 or posicao > self._tamaho:
+            return
+        #atual andará na lista
+        atual = self.inicio
+        for _ in range(posicao - 2):
+            atual = atual.prox
+        return atual.prox
+
     def adicionar_inicio(self, valor):
         self._tamanho+=1
         if self._inicio is None:
             self._inicio = No(valor)
             return
         
+        # caso já tenha algum elemento na lista
         novo_no = No(valor)
-        novo_no.proximo = self._inicio
+        novo_no.prox = self._inicio
         self._inicio = novo_no
 
-    def adicionar_final(self):
+    def adicionar_final(self, valor):
         self._tamanho+=1
-        pass
-    def adicionar_indice(self):
-        self._tamanho+=1
-        pass
 
+        if self._inicio is None:
+            self._inicio = No(valor)
+            return
+        
+        # caso já tenha algum elemento na lista
+        atual = self._inicio
+        # vai fazer um loop até o final depois add no final o valor
+        while (atual.prox is not None):
+            atual = atual.prox
+        atual.prox = No(valor)
+
+    def adicionar_indice(self, posicao, valor):
+        self._tamanho+=1
+        # começa a contar do 1
+        if self._inicio is None and posicao == 1:
+            # add no inicio da lista
+            self._inicio = No(valor)
+            return
+        # para evitar erros
+        if posicao < 1 or posicao > self._tamanho:
+            return
+        
+        atual = self._inicio
+        # vai percorrer a listag
+        for _ in range(posicao - 2):
+            atual = atual.prox 
+        novo_no = No(valor) #recebendo o no
+        novo_no.prox = atual.prox  # agora o prox do novo No vai receber None
+        atual.prox = novo_no # o None atual receberá o Valor no novo No, e depois dele terá um prox com valor None
+
+    def __str__(self):
+            p = self._inicio
+            string = ""
+            while(p != None):
+                string += p.valor
+                p = p.prox
+            return string    
+        
+#-----------------------------
+if __name__ == "__main__":
+    lista = ListaEncadeada()
+    lista.adicionar_final("A")
+    lista.adicionar_final("B")
+    lista.adicionar_final("B")
+    lista.adicionar_final("B")
+    lista.adicionar_final("B")
+    
+
+    print(lista)
+        
+
+
+        
     
--- Lista_Encadeada/No.py
+++ Lista_Encadeada/No.py
@@ -1,8 +1,8 @@
 class No:
     def __init__(self, valor):
         self._valor = valor
-        self.prox = None
-    
+        self._prox = None
+
     @property
     def valor(self):
         return self._valor
@@ -15,6 +15,28 @@ class No:
     def prox(self):
         return self._prox
     
+    @prox.setter
+    def prox(self, prox):
+        self._prox = prox
+
+
+class No_teste:
+    def __init__(self, dado):
+        self._dado = dado
+        self._prox = None
+    
+    @property
+    def dado(self):
+        return self._dado
+    
+    @dado.setter
+    def dado(self, dado):
+        self._dado = dado
+    
+    @property
+    def prox(self):
+        return self._prox
+
     @prox.setter
     def prox(self, prox):
         self._prox = prox
